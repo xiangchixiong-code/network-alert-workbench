b5cdddce03083da5762c407e5ffc74c3308dfedb
xiangchixiong-code <xiangchixiong@gmail.com>
2026-05-31 04:13:44 +0800

    add src/main/java/com/xianggon/netalerts/common/ResourceNotFoundException.java

---
+++ src/main/java/com/xianggon/netalerts/common/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.xianggon.netalerts.common;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
