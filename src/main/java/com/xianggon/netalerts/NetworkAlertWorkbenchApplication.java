6a050848efdd1388cb436abfb156568553d89a42
xiangchixiong-code <xiangchixiong@gmail.com>
2026-05-31 04:13:40 +0800

    add src/main/java/com/xianggon/netalerts/NetworkAlertWorkbenchApplication.java

---
+++ src/main/java/com/xianggon/netalerts/NetworkAlertWorkbenchApplication.java
@@ -0,0 +1,11 @@
+package com.xianggon.netalerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkAlertWorkbenchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkAlertWorkbenchApplication.class, args);
+    }
+}
